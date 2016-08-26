aeccc9ba425d5e24bb57e396061207902e953ba3
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-08-26 22:26:26 +0400

    restyling in Liabilities section

--- client/components/Liabilities/Liabilities.js
+++ client/components/Liabilities/Liabilities.js
@@ -10,7 +10,28 @@ export default class Liabilities extends Component {
     return (
       <div className="liabilities">
         <div className="title">Liabilities</div>
-        todo
+        <table className="table">
+          <thead>
+            <tr>
+              <td>Type</td>
+              <td>Lender</td>
+              <td>Limit</td>
+              <td>Balance</td>
+              <td>Monthly Payment</td>
+            </tr>
+          </thead>
+          <tbody>
+            {liabilities.map((liability, index) => (
+              <tr key={index}>
+                <td>{liability.type}</td>
+                <td>{liability.lender}</td>
+                <td>{liability.limit}</td>
+                <td>{liability.balance}</td>
+                <td>{liability.monthlyPayment}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
         <style>{styles}</style>
       </div>
     );
