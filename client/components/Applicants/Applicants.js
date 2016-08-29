9e7727522eaa08b879a9720fad4a8216a7577d93
Rishat Muhametshin <rishatmuhametshin@gmail.com>
2016-08-29 21:00:43 +0400

    2-column table when 2 applicants

--- client/components/Applicants/Applicants.js
+++ client/components/Applicants/Applicants.js
@@ -11,71 +11,86 @@ export default class Applicants extends Component {
       applicants = []
     } = this.props;
 
-    return (
-      <div className="applicants">
-        <div className="section-title">Applicants</div>
-        {applicants.map((applicant, index) => (
-          <div
-            key={index}
-            className="applicant">
+    if (applicants.length === 2) {
+      const left = applicants[0];
+      const right = applicants[1];
+
+      return (
+        <div className="applicants">
+          <div className="section-title">Applicants</div>
+          <div className="applicant">
             <div className="table-title">Personal Details</div>
             <table className="table">
               <tbody>
                 <tr>
                   <th>Title</th>
-                  <td>{applicant.title}</td>
+                  <td>{left.title}</td>
+                  <td>{right.title}</td>
                 </tr>
                 <tr>
                   <th>First Name</th>
-                  <td>{applicant.first_name}</td>
+                  <td>{left.first_name}</td>
+                  <td>{right.first_name}</td>
                 </tr>
                 <tr>
                   <th>Middle Name</th>
-                  <td>{applicant.middle_name}</td>
+                  <td>{left.middle_name}</td>
+                  <td>{right.middle_name}</td>
                 </tr>
                 <tr>
                   <th>Surname</th>
-                  <td>{applicant.surname}</td>
+                  <td>{left.surname}</td>
+                  <td>{right.surname}</td>
                 </tr>
                 <tr>
                   <th>Date of Birth</th>
-                  <td>{moment(applicant.date_of_birth).format(DATE_FORMAT)}</td>
+                  <td>{moment(left.date_of_birth).format(DATE_FORMAT)}</td>
+                  <td>{moment(right.date_of_birth).format(DATE_FORMAT)}</td>
                 </tr>
                 <tr>
                   <th>Mother's Maiden Name</th>
-                  <td>{applicant.mother_maiden_name}</td>
+                  <td>{left.mother_maiden_name}</td>
+                  <td>{right.mother_maiden_name}</td>
                 </tr>
                 <tr>
                   <th>Marital Status</th>
-                  <td>{applicant.marital_status}</td>
+                  <td>{left.marital_status}</td>
+                  <td>{right.marital_status}</td>
                 </tr>
                 <tr>
                   <th>Ages of Dependants</th>
-                  <td>{applicant.ages_of_dependants}</td>
+                  <td>{left.ages_of_dependants}</td>
+                  <td>{right.ages_of_dependants}</td>
                 </tr>
                 <tr>
                   <th>Phone No. (h/w/m)</th>
-                  <td>{applicant.phone_no}</td>
+                  <td>{left.phone_no}</td>
+                  <td>{right.phone_no}</td>
                 </tr>
                 <tr>
                   <th>Email Address</th>
-                  <td>{applicant.email_address}</td>
+                  <td>{left.email_address}</td>
+                  <td>{right.email_address}</td>
                 </tr>
                 <tr>
                   <th>Other ID</th>
-                  <td>{applicant.other_id}</td>
+                  <td>{left.other_id}</td>
+                  <td>{right.other_id}</td>
                 </tr>
                 <tr>
                   <th>First Home Owner</th>
-                  <td>{applicant.first_home_owner ? 'Yes' : 'No'}</td>
+                  <td>{left.first_home_owner ? 'Yes' : 'No'}</td>
+                  <td>{right.first_home_owner ? 'Yes' : 'No'}</td>
                 </tr>
                 <tr>
                   <th>Aust Perm Resident?</th>
-                  <td>{applicant.aust_perm_resident ? 'Yes' : 'No'}</td>
+                  <td>{left.aust_perm_resident ? 'Yes' : 'No'}</td>
+                  <td>{right.aust_perm_resident ? 'Yes' : 'No'}</td>
                 </tr>
                 <tr>
                   <th>Dependant Spouse</th>
-                  <td>{applicant.dependant_spouse ? 'Yes' : 'No'}</td>
+                  <td>{left.dependant_spouse ? 'Yes' : 'No'}</td>
+                  <td>{right.dependant_spouse ? 'Yes' : 'No'}</td>
                 </tr>
               </tbody>
             </table>
@@ -84,27 +99,33 @@ export default class Applicants extends Component {
               <tbody>
                 <tr>
                   <th>Current Address</th>
-                  <td>{applicant.current_address}</td>
+                  <td>{left.current_address}</td>
+                  <td>{right.current_address}</td>
                 </tr>
                 <tr>
                   <th>Date Moved In</th>
-                  <td>{moment(applicant.date_moved_in).format(DATE_FORMAT)}</td>
+                  <td>{moment(left.date_moved_in).format(DATE_FORMAT)}</td>
+                  <td>{moment(right.date_moved_in).format(DATE_FORMAT)}</td>
                 </tr>
                 <tr>
                   <th>Residental Status</th>
-                  <td>{applicant.residental_status}</td>
+                  <td>{left.residental_status}</td>
+                  <td>{right.residental_status}</td>
                 </tr>
                 <tr>
                   <th>Postal Address (if different)</th>
-                  <td>{applicant.postal_address}</td>
+                  <td>{left.postal_address}</td>
+                  <td>{right.postal_address}</td>
                 </tr>
                 <tr>
                   <th>Previous Address</th>
-                  <td>{applicant.previous_address}</td>
+                  <td>{left.previous_address}</td>
+                  <td>{right.previous_address}</td>
                 </tr>
                 <tr>
                   <th>Date / Prev. Add.</th>
-                  <td>{moment(applicant.date_moved_in_pr_add).format(DATE_FORMAT)}</td>
+                  <td>{moment(left.date_moved_in_pr_add).format(DATE_FORMAT)}</td>
+                  <td>{moment(right.date_moved_in_pr_add).format(DATE_FORMAT)}</td>
                 </tr>
               </tbody>
             </table>
@@ -113,53 +134,215 @@ export default class Applicants extends Component {
               <tbody>
                 <tr>
                   <th>Current Employer's Name</th>
-                  <td>{applicant.current_employes_name}</td>
+                  <td>{left.current_employes_name}</td>
+                  <td>{right.current_employes_name}</td>
                 </tr>
                 <tr>
                   <th>Start Date</th>
-                  <td>{moment(applicant.start_date).format(DATE_FORMAT)}</td>
+                  <td>{moment(left.start_date).format(DATE_FORMAT)}</td>
+                  <td>{moment(right.start_date).format(DATE_FORMAT)}</td>
                 </tr>
                 <tr>
                   <th>Status</th>
-                  <td>{applicant.status}</td>
+                  <td>{left.status}</td>
+                  <td>{right.status}</td>
                 </tr>
                 <tr>
                   <th>Job Title</th>
-                  <td>{applicant.job_title}</td>
+                  <td>{left.job_title}</td>
+                  <td>{right.job_title}</td>
                 </tr>
                 <tr>
                   <th>Gross Annual Salary</th>
-                  <td>{applicant.gross_annual_salary}</td>
+                  <td>{left.gross_annual_salary}</td>
+                  <td>{right.gross_annual_salary}</td>
                 </tr>
                 <tr>
                   <th>Commission</th>
-                  <td>{applicant.commission}</td>
+                  <td>{left.commission}</td>
+                  <td>{right.commission}</td>
                 </tr>
                 <tr>
                   <th>Employer Address</th>
-                  <td>{applicant.employer_address}</td>
+                  <td>{left.employer_address}</td>
+                  <td>{right.employer_address}</td>
                 </tr>
                 <tr>
                   <th>Name and Number</th>
-                  <td>{applicant.contact_details}</td>
+                  <td>{left.contact_details}</td>
+                  <td>{right.contact_details}</td>
                 </tr>
                 <tr>
                   <th>Rental Income</th>
-                  <td>{applicant.rental_income}</td>
+                  <td>{left.rental_income}</td>
+                  <td>{right.rental_income}</td>
                 </tr>
                 <tr>
                   <th>Family Tax A/B</th>
-                  <td>{applicant.family_tax}</td>
+                  <td>{left.family_tax}</td>
+                  <td>{right.family_tax}</td>
                 </tr>
                 <tr>
                   <th>Other Income</th>
-                  <td>{applicant.other_income}</td>
+                  <td>{left.other_income}</td>
+                  <td>{right.other_income}</td>
                 </tr>
               </tbody>
             </table>
           </div>
-        ))}
-      </div>
-    );
+        </div>
+      );
+    } else {
+      return (
+        <div className="applicants">
+          <div className="section-title">Applicants</div>
+          {applicants.map((applicant, index) => (
+            <div
+              key={index}
+              className="applicant">
+              <div className="table-title">Personal Details</div>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th>Title</th>
+                    <td>{applicant.title}</td>
+                  </tr>
+                  <tr>
+                    <th>First Name</th>
+                    <td>{applicant.first_name}</td>
+                  </tr>
+                  <tr>
+                    <th>Middle Name</th>
+                    <td>{applicant.middle_name}</td>
+                  </tr>
+                  <tr>
+                    <th>Surname</th>
+                    <td>{applicant.surname}</td>
+                  </tr>
+                  <tr>
+                    <th>Date of Birth</th>
+                    <td>{moment(applicant.date_of_birth).format(DATE_FORMAT)}</td>
+                  </tr>
+                  <tr>
+                    <th>Mother's Maiden Name</th>
+                    <td>{applicant.mother_maiden_name}</td>
+                  </tr>
+                  <tr>
+                    <th>Marital Status</th>
+                    <td>{applicant.marital_status}</td>
+                  </tr>
+                  <tr>
+                    <th>Ages of Dependants</th>
+                    <td>{applicant.ages_of_dependants}</td>
+                  </tr>
+                  <tr>
+                    <th>Phone No. (h/w/m)</th>
+                    <td>{applicant.phone_no}</td>
+                  </tr>
+                  <tr>
+                    <th>Email Address</th>
+                    <td>{applicant.email_address}</td>
+                  </tr>
+                  <tr>
+                    <th>Other ID</th>
+                    <td>{applicant.other_id}</td>
+                  </tr>
+                  <tr>
+                    <th>First Home Owner</th>
+                    <td>{applicant.first_home_owner ? 'Yes' : 'No'}</td>
+                  </tr>
+                  <tr>
+                    <th>Aust Perm Resident?</th>
+                    <td>{applicant.aust_perm_resident ? 'Yes' : 'No'}</td>
+                  </tr>
+                  <tr>
+                    <th>Dependant Spouse</th>
+                    <td>{applicant.dependant_spouse ? 'Yes' : 'No'}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="table-title">Address Details</div>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th>Current Address</th>
+                    <td>{applicant.current_address}</td>
+                  </tr>
+                  <tr>
+                    <th>Date Moved In</th>
+                    <td>{moment(applicant.date_moved_in).format(DATE_FORMAT)}</td>
+                  </tr>
+                  <tr>
+                    <th>Residental Status</th>
+                    <td>{applicant.residental_status}</td>
+                  </tr>
+                  <tr>
+                    <th>Postal Address (if different)</th>
+                    <td>{applicant.postal_address}</td>
+                  </tr>
+                  <tr>
+                    <th>Previous Address</th>
+                    <td>{applicant.previous_address}</td>
+                  </tr>
+                  <tr>
+                    <th>Date / Prev. Add.</th>
+                    <td>{moment(applicant.date_moved_in_pr_add).format(DATE_FORMAT)}</td>
+                  </tr>
+                </tbody>
+              </table>
+              <div className="table-title">Income Details</div>
+              <table className="table">
+                <tbody>
+                  <tr>
+                    <th>Current Employer's Name</th>
+                    <td>{applicant.current_employes_name}</td>
+                  </tr>
+                  <tr>
+                    <th>Start Date</th>
+                    <td>{moment(applicant.start_date).format(DATE_FORMAT)}</td>
+                  </tr>
+                  <tr>
+                    <th>Status</th>
+                    <td>{applicant.status}</td>
+                  </tr>
+                  <tr>
+                    <th>Job Title</th>
+                    <td>{applicant.job_title}</td>
+                  </tr>
+                  <tr>
+                    <th>Gross Annual Salary</th>
+                    <td>{applicant.gross_annual_salary}</td>
+                  </tr>
+                  <tr>
+                    <th>Commission</th>
+                    <td>{applicant.commission}</td>
+                  </tr>
+                  <tr>
+                    <th>Employer Address</th>
+                    <td>{applicant.employer_address}</td>
+                  </tr>
+                  <tr>
+                    <th>Name and Number</th>
+                    <td>{applicant.contact_details}</td>
+                  </tr>
+                  <tr>
+                    <th>Rental Income</th>
+                    <td>{applicant.rental_income}</td>
+                  </tr>
+                  <tr>
+                    <th>Family Tax A/B</th>
+                    <td>{applicant.family_tax}</td>
+                  </tr>
+                  <tr>
+                    <th>Other Income</th>
+                    <td>{applicant.other_income}</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          ))}
+        </div>
+      );
+    }
   }
 }
--- client/components/Application/Application.js
+++ client/components/Application/Application.js
@@ -1,114 +1,46 @@
 import React, { Component } from 'react';
 
+const labels = {
+  better_rate: 'Better Rate',
+  end_of_fixed_rate: 'End of Fixed Rate',
+  debt_consolidation: 'Debt Consolidation',
+  additional_funds: 'Additional Funds',
+  construction: 'Construction',
+  lock_in_equity: 'Lock in Equity',
+  dislikes_lender: 'Dislikes Lender',
+  change_structure: 'Change Structure',
+  income_rate: 'Income Rate',
+  income_speed: 'Income Speed',
+  valuation: 'Valuation',
+  property_type: 'Property Type',
+  construction_purchase: 'Construction Purchase',
+  preapproval: 'Preapproval',
+  income_increase: 'Income Increase',
+  extra_earner: 'Extra Earner',
+  add_inc_source: 'Add Inc Source',
+  income_reduce: 'Income Reduce',
+  school_fees: 'School Fees'
+};
+
 export default class Application extends Component {
   render() {
     const {
       application: {
-        better_rate,
-        end_of_fixed_rate,
-        debt_consolidation,
-        additional_funds,
-        construction,
-        lock_in_equity,
-        dislikes_lender,
-        change_structure,
-        income_rate,
-        income_speed,
-        valuation,
-        property_type,
-        construction_purchase,
-        preapproval,
-        income_increase,
-        extra_earner,
-        add_inc_source,
-        income_reduce,
-        school_fees,
-        additional_comments
+        additional_comments,
+        ...application
       } = {}
     } = this.props;
 
     return (
       <div className="application">
-        <table className="table">
-          <tbody>
-            <tr>
-              <th>Better Rate</th>
-              <td>{better_rate ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>End of Fixed Rate</th>
-              <td>{end_of_fixed_rate ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Debt Consolidation</th>
-              <td>{debt_consolidation ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Additional Funds</th>
-              <td>{additional_funds ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Construction</th>
-              <td>{construction ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Lock in Equity</th>
-              <td>{lock_in_equity ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Dislikes Lender</th>
-              <td>{dislikes_lender ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Change Structure</th>
-              <td>{change_structure ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Income Rate</th>
-              <td>{income_rate ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Income Speed</th>
-              <td>{income_speed ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Valuation</th>
-              <td>{valuation ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Property Type</th>
-              <td>{property_type ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Construction Purchase</th>
-              <td>{construction_purchase ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Preapproval</th>
-              <td>{preapproval ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Income Increase</th>
-              <td>{income_increase ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Extra Earner</th>
-              <td>{extra_earner ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Add Inc Source</th>
-              <td>{add_inc_source ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>Income Reduce</th>
-              <td>{income_reduce ? 'Yes' : 'No'}</td>
-            </tr>
-            <tr>
-              <th>School Fees</th>
-              <td>{school_fees ? 'Yes' : 'No'}</td>
-            </tr>
-          </tbody>
-        </table>
+        <div className="table-title">Objectives</div>
+        <ol>
+          {Object.keys(labels).filter(key => application[key]).map((key, index) => (
+            <li key={index}>
+              {labels[key]}
+            </li>
+          ))}
+        </ol>
         <div className={additional_comments ? 'section-title' : 'hidden'}>Additional Comments</div>
         <div>{additional_comments}</div>
       </div>
